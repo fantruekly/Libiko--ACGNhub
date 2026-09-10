afc67b7b3c73d49f9266ebe418b1dd20611f8eeb
true fan <fantruekly@gmail.com>
2026-09-10 22:32:21 +0800

    feat(anime): expose metadata service and feed provider

--- lib/modules/anime/anime_providers.dart
+++ lib/modules/anime/anime_providers.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
+import '../../core/metadata/metadata_provider.dart';
+import '../../core/metadata/metadata_service.dart';
 import '../../core/source/source_manager.dart';
 import '../../core/models/work.dart';
 import 'anime_source.dart';
@@ -22,6 +24,12 @@ final animeSourceListProvider = FutureProvider<List<AnimeSource>>((ref) async {
     manager.register(AnimeSource(rule));
   }
   return manager.getByType(WorkType.anime).cast<AnimeSource>();
+});
+
+final metadataServiceProvider = Provider<MetadataService>((ref) => MetadataService());
+
+final animeFeedProvider = FutureProvider.family<List<Work>, AnimeFeed>((ref, feed) {
+  return ref.watch(metadataServiceProvider).feed(feed);
 });
 
 String _stableCoverUrl(String? cover) {
