c84f8373eb255a52bc4c315984a4c6b1001621fa
true fan <fantruekly@gmail.com>
2026-09-11 20:18:20 +0800

    feat(video): add media_kit player page

---
+++ lib/modules/anime/video_player_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:media_kit/media_kit.dart';
+import 'package:media_kit_video/media_kit_video.dart';
+
+class VideoPlayerPage extends StatefulWidget {
+  final String title;
+  final String streamUrl;
+
+  const VideoPlayerPage({super.key, required this.title, required this.streamUrl});
+
+  @override
+  State<VideoPlayerPage> createState() => _VideoPlayerPageState();
+}
+
+class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  late final Player _player;
+  late final VideoController _controller;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _player = Player();
+    _controller = VideoController(_player);
+    _player.stream.error.listen((e) {
+      if (mounted) setState(() => _error = e);
+    });
+    _player.open(Media(widget.streamUrl));
+  }
+
+  @override
+  void dispose() {
+    _player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: Text(widget.title, maxLines: 1, overflow: TextOverflow.ellipsis),
+      ),
+      body: Center(
+        child: _error != null
+            ? Padding(
+                padding: const EdgeInsets.all(24),
+                child: Text('播放失败：$_error', style: const TextStyle(color: Colors.white70)),
+              )
+            : Video(controller: _controller),
+      ),
+    );
+  }
+}
