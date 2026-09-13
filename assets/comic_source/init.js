0d1a2834c3f7341b36e3256183aa71e476cc380d
true fan <fantruekly@gmail.com>
2026-09-13 19:59:48 +0800

    fix(comic): define the createUuid global for picacg login

--- assets/comic_source/init.js
+++ assets/comic_source/init.js
@@ -162,6 +162,14 @@
     };
   };
 
+  globalThis.createUuid = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = (Math.random() * 16) | 0;
+      const v = c === 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  };
+
   globalThis.APP = {
     version: '1.5.0',
     locale: 'zh_CN',
