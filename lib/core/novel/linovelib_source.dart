9330236b66f126f8f0b28ef1f5d1f06af7d97212
true fan <fantruekly@gmail.com>
2026-09-14 19:30:29 +0800

    fix(novel): load linovelib bunko via the mobile host

--- lib/core/novel/linovelib_source.dart
+++ lib/core/novel/linovelib_source.dart
@@ -6,6 +6,7 @@ import 'models.dart';
 import 'novel_source.dart';
 
 const String linovelibBaseUrl = 'https://www.linovelib.com';
+const String linovelibMobileBaseUrl = 'https://w.linovelib.com';
 const String linovelibUserAgent =
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
     '(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
@@ -61,6 +62,46 @@ List<Novel> parseBookList(String html) {
     if (n != null) out.add(n);
   }
   return out;
+}
+
+List<Novel> parseMobileBookList(String html) {
+  final doc = html_parser.parse(html);
+  final out = <Novel>[];
+  for (final li in doc.querySelectorAll('ol.book-ol li.book-li')) {
+    final a = li.querySelector('a.book-layout');
+    final id = novelIdFromHref(a?.attributes['href']);
+    if (a == null || id == null) continue;
+    final img = li.querySelector('div.book-cover img');
+    final cover =
+        _absUrl(img?.attributes['data-src'] ?? img?.attributes['src']);
+    final title = _textOf(li.querySelector('h4.book-title'));
+    final authorEl = li.querySelector('span.book-author');
+    authorEl?.querySelector('svg')?.remove();
+    final author = _textOf(authorEl);
+    final tags = <String>[];
+    final tagEl = li.querySelector('em.tag-small.yellow');
+    if (tagEl != null) {
+      tags.addAll(_textOf(tagEl)
+          .split(RegExp(r'\s+'))
+          .where((e) => e.isNotEmpty));
+    }
+    out.add(Novel(
+      id: id,
+      title: title,
+      author: author.isEmpty ? null : author,
+      coverUrl: cover.isEmpty ? null : cover,
+      tags: tags,
+      extra: {'url': '$linovelibBaseUrl/novel/$id.html'},
+    ));
+  }
+  return out;
+}
+
+bool mobileHasNextPage(String html, int page) {
+  final doc = html_parser.parse(html);
+  final max = int.tryParse(_textOf(doc.querySelector('div.pagelink a.last')));
+  if (max == null) return false;
+  return page < max;
 }
 
 List<NovelSection> parseHome(String html) {
@@ -341,11 +382,12 @@ class LinovelibSource implements NovelSource {
   @override
   Future<NovelList> browse(String optionKey, {int page = 1}) async {
     final isRanking = rankingKeys.contains(optionKey);
-    final path = browsePath(optionKey, page);
-    final html = await _get(path);
-    final items = isRanking ? parseRankRows(html) : parseBookList(html);
-    final hasMore =
-        hasPaginationControl(html) ? hasNextPage(html) : items.length >= 10;
+    final html = await _getUrl(browsePath(optionKey, page));
+    final items =
+        isRanking ? parseRankRows(html) : parseMobileBookList(html);
+    final hasMore = isRanking
+        ? (hasPaginationControl(html) ? hasNextPage(html) : items.length >= 10)
+        : mobileHasNextPage(html, page);
     return NovelList(items: items, page: page, hasMore: hasMore);
   }
 
@@ -355,8 +397,8 @@ class LinovelibSource implements NovelSource {
 
   static String browsePath(String optionKey, int page) =>
       rankingKeys.contains(optionKey)
-          ? rankPath(optionKey, page)
-          : bunkoPath(optionKey, page);
+          ? '$linovelibBaseUrl${rankPath(optionKey, page)}'
+          : '$linovelibMobileBaseUrl/wenku/$optionKey/$page.html';
 
   Future<String> _get(String path) async {
     final res = await _dio.get<String>(
@@ -366,6 +408,18 @@ class LinovelibSource implements NovelSource {
     final data = res.data;
     if (res.statusCode != 200 || data == null) {
       throw Exception('linovelib 请求失败：$path (${res.statusCode})');
+    }
+    return data;
+  }
+
+  Future<String> _getUrl(String url) async {
+    final res = await _dio.get<String>(
+      url,
+      options: Options(responseType: ResponseType.plain),
+    );
+    final data = res.data;
+    if (res.statusCode != 200 || data == null) {
+      throw Exception('linovelib 请求失败：$url (${res.statusCode})');
     }
     return data;
   }
---
+++ test/core/novel/linovelib_mobile_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:acgnhub/core/novel/linovelib_source.dart';
+
+const _mobileHtml = '''
+<ol class="book-ol book-ol-normal">
+  <li class="book-li"><a href="/novel/22.html" class="book-layout">
+    <div class="book-cover"><img src="x.svg" data-src="https://www.bilinovel.com/files/article/image/0/22/22s.jpg" class="lazyload" alt="加速世界"></div>
+    <div class="book-cell">
+      <div class="book-title-x"><h4 class="book-title">加速世界</h4></div>
+      <p class="book-desc">简介</p>
+      <div class="book-meta">
+        <div class="book-meta-l"><span class="book-author"><svg class="icon icon-human"><title>作者</title><use xlink:href="#icon-human"></use></svg>川原砾</span></div>
+        <div class="book-meta-r"><span class="tag-small-group"><em class="tag-small yellow">校园 科幻</em><em class="tag-small red">连载</em></span></div>
+      </div>
+    </div>
+  </a></li>
+</ol>
+<div class="pagelink" id="pagelink"><a href="/wenku/dengekibunko/1.html" class="first">1</a><strong>1</strong><a href="/wenku/dengekibunko/2.html">2</a><a href="/wenku/dengekibunko/21.html" class="last">21</a></div>
+''';
+
+void main() {
+  test('parseMobileBookList reads mobile book cards', () {
+    final items = parseMobileBookList(_mobileHtml);
+    expect(items, hasLength(1));
+    final n = items.single;
+    expect(n.id, '22');
+    expect(n.title, '加速世界');
+    expect(n.coverUrl, 'https://www.bilinovel.com/files/article/image/0/22/22s.jpg');
+    expect(n.author, '川原砾');
+    expect(n.tags, ['校园', '科幻']);
+  });
+
+  test('mobileHasNextPage uses the last page link', () {
+    expect(mobileHasNextPage(_mobileHtml, 1), isTrue);
+    expect(mobileHasNextPage(_mobileHtml, 20), isTrue);
+    expect(mobileHasNextPage(_mobileHtml, 21), isFalse);
+  });
+}
